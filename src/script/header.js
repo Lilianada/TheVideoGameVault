bbdfc36e2e875a65c263777cee557abc4820de0b
Lilianada <lilibest30.lb@gmail.com>
2023-01-28 17:20:27 -0500

    Articles section

--- src/script/header.js
+++ src/script/header.js
@@ -1,10 +1,10 @@
 // SHOW SCROLL TOP
-function scrollTop() {
-  const scrollTop = document.getElementById("scroll-top");
-  if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
-  else scrollTop.classList.remove("show-scroll");
-}
-window.addEventListener("scroll", scrollTop);
+// function scrollTop() {
+//   const scrollTop = document.getElementById("scroll-top");
+//   if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
+//   else scrollTop.classList.remove("show-scroll");
+// }
+// window.addEventListener("scroll", scrollTop);
 
 class myHeader extends HTMLElement {
   connectedCallback() {
