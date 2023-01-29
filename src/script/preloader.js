061c921af5ea3ffc5a935d48b4679ccb91fecb69
Lilianada <lilibest30.lb@gmail.com>
2023-01-29 12:29:09 -0500

    ....fixes

---
+++ src/script/preloader.js
@@ -0,0 +1,8 @@
+{/* <div className="preloader">
+        <span className="preloadText">
+           <span class="preload-dot"></span>
+           <span class="preload-dot"></span>
+           <span class="preload-dot"></span>
+           <span class="preload-dot"></span>
+        </span>
+      </div> */}
